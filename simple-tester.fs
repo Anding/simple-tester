8274dc3e60b2ff72437042013bec25270714e20c
Andrew Read <andrew81244@outlook.com>
2019-06-15 18:23:42 +0800

    Comments updated

--- simple-tester.fs
+++ simple-tester.fs
@@ -1,44 +1,53 @@
-\ simple-tester is a unit tester intended for embedded systems or other simple targets
+\ simple-tester is a unit tester intended for embedded systems or other hardware targets
 \ based on the ANS Forth version of John Hayes' Forth ttester
+
+\ the purpose of simple-tester is
+\ (i)	to allow unit testing on targets with limited resources and
+\ (ii)	to allow testing as early on as possible in the lifecycle of a new forth system,
+\		even before the system knows how to compile new word defintions
 \
 \ simple-tester requires
-\ (i)    a numeric output device (e.g. a 4 character 7-segment LCD)
+\ (i)    a numeric output device (e.g. a seven-segment display)
 \ (ii)   a system halt instruction
 \ (iii ) the forth word DEPTH
 
-\ The usage format has been changed from ttester with the intention of being more "forth-like"
-\ T{ run-a-module-of-code }T x1 x2 ... xn ==
-\ T{ and }T bracket the code being tested
+\ The usage format has been changed from ttester with the hope/intention of being more "forth-like"
+\ T{ module-of-code }T x1 x2 ... xn ==
+\ T{ and }T brackets the code being tested
 \ x1, x2, ... xn are the expected outputs
-\ == is the assert instruction that performs the comparison and indicates the result
+\ == is the assert instruction that performs the comparison and halts the system on the first fail
+\ if a test either fails to complete execution or the actual and expected outputs do not match, then
+\ the numeric output device will show the number of the test that failed
 
 \ simple-tester uses a hash algorithm to compare inputs and outputs rather than cell by cell comparison
 \ possible advantages
-\ (i) 	2 cells only of RAM required
-\ (ii)	testing limited only by stack depth
-\ (iii) simple comparison code without stack gynmastics, easy to write the necessary code words
-\ (iv)	fast, advantageous for POST applications
-\ possible disadvantages
-\ (i)	false negatives - hash collissions may pass a test that should actually have been failed
+\ (i) 	2 cells only of RAM required for operation
+\ (ii)	testing scope is limited only by stack depth
+\ (iii) no stack gynmastics, easy to write the necessary code words in brief assembly language
+\ (iv)	fast, advantageous for power-on-self-test-applications
+\ disadvantage
+\ (i)	false positive risk - hash collissions may allow a test to pass that should actually have been failed
 
-\ on an embedded system the following words are expected to be available as code words
-\ (thus testing of a new forth system can begin even before the system knows how to compile new word definitions)
-\ words are coded here in forth for development and to give the pseudocode for translation into code
+\ the following words are expected to be available as code words on the target system, other words are utility words
+\ Tstart Tend T{ T} ==
 
-\ report the test number to a numeric output device
+\ utility words called by the unit test code words
+\ ===========================================================================================================
+
+\ report the test number to a numeric output device, such as a seven-segment display
 : T.
-	.			\ for gforth
+	.			\ for gforth testing
 ;
 
 \ halt the system
 : halt
-	quit		\ for gforth
+	quit		\ for gforth testing
 ;
 
 \ compute h1 by hashing x1 and h0
 : hash ( x1 h0 -- h1)
-	swap 1+ xor									\ hash may be a simple function initially but upgraded later
-;												\ to improve collision detection and test reliability
+	swap 1+ xor									\ hash may be any simple function initially but upgraded later
+;												\ 	to minimize the risk of collisions
 
 \ hash n items from the stack and return the hash code
 : hash-n ( x1 x2 ... xn n -- h)
@@ -55,32 +64,36 @@
 variable Tcount
 variable Tdepth
 
+\ unit test code words to be suitably implemented on the target system
+\ reference implementations here in Forth
+\ ===========================================================================================================
+
 \ start testing
 : Tstart
-0 Tcount !
+	0 Tcount !
 ;
 
 \ start a unit test
-: T{ ( N R:d)
+: T{ ( )
 	Tcount @ 1+ dup T. Tcount !					\ increment and report the test number
 	depth Tdepth !								\ save the stack depth before the module runs
 ;
 
-\ finish a unit test, y1, y2 ... yn are the actual outputs
-: }T ( N y1 y2 ... yn R:d -- N hy R:d)
-	depth Tdepth @ -	( N y1 y2 ... yn Ny)	\ Ny  = no. outputs created by running the module
-	hash-n				( N hy)
-	depth Tdepth !		( N hy)					\ save the stack depth before the expected outputs
+\ finish a unit test,
+: }T ( y1 y2 ... yn -- hy) 						\ y1, y2 ... yn are the actual outputs
+	depth Tdepth @ -	( y1 y2 ... yn Ny)		\ Ny  = no. outputs created by running the module
+	hash-n				( hy)					\ hy = hash value of the actual outputs
+	depth Tdepth !		( hy)					\ save the stack depth before the expected outputs
 ;
 
 \ compare actual output with expected output
-: == ( N h x1 x2 ... xn R:d -- N)
-	depth Tdepth @ -	( N hy x1 x2 .. xn Nx)	\ Nx = no. outputs expected
-	hash-n				( N hy hx)
+: == ( hy x1 x2 ... xn --)
+	depth Tdepth @ -	( hy x1 x2 .. xn Nx)	\ Nx = no. outputs expected
+	hash-n				( hy hx)				\ hx = hash value of the expected outputs
 	= 0= IF halt THEN							\ hash codes didn't match, stop the system
 ;
 
 \ signal end of testing
-: Tend  ( N --)
-	65535 T.									\ some at-a-glance value to indicate successful completion
+: Tend  ( --)
+	65535 ( 0xFFFF) T.							\ some at-a-glance value to indicate successful completion
 ;
